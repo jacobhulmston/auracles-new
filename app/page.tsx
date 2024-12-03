7cb9d860599cb3079ae6f504a226b935d39b36c1
Jacob <jacobhulmston@gmail.com>
2024-12-03 20:23:27 +0000

    style: reduce theme delay

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsDark(true);
-    }, 1000);
+    }, 100);
 
     return () => clearTimeout(timer);
   }, []);
